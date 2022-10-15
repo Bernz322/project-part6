234986623c66d2a54bf8ff4706bd0bb9d64942a4
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-15 21:50:31 +0800

    create store and rtk slice for api and state mngmt

---
+++ frontend/src/features/auth/authSlice.ts
@@ -0,0 +1,128 @@
+import {
+  ActionReducerMapBuilder,
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import { login, register } from "../../utils/apiCalls";
+import { deleteCookie } from "../../utils/helpers";
+
+export interface AuthState {
+  isError: boolean;
+  isSuccess: boolean;
+  isLoading: boolean;
+  message: any;
+}
+
+const initialState: AuthState = {
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Login User
+export const authLogin = createAsyncThunk(
+  "auth/login",
+  async (data: { email: string; password: string }, thunkAPI) => {
+    try {
+      const res = await login(data.email, data.password);
+      return res;
+    } catch (error: any) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Register User
+export const authRegister = createAsyncThunk(
+  "auth/register",
+  async (data: { name: string; email: string; password: string }, thunkAPI) => {
+    try {
+      const res = await register(data.name, data.email, data.password);
+      return res;
+    } catch (error: any) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      toast.error(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Logout User
+export const authLogout = createAsyncThunk("auth/logout", async () => {
+  deleteCookie({
+    cookieName: "accessToken",
+    path: "/",
+    domain: "localhost",
+  });
+  localStorage.removeItem("loggedUser");
+  // TODO: Reset all states here
+});
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    authReset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder: ActionReducerMapBuilder<AuthState>) => {
+    builder
+      .addCase(authLogin.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.isSuccess = false;
+        state.message = "";
+      })
+      .addCase(authLogin.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = "";
+      })
+      .addCase(authLogin.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.payload;
+      })
+      .addCase(authRegister.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.isSuccess = false;
+        state.message = "";
+      })
+      .addCase(authRegister.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        state.message = "";
+      })
+      .addCase(authRegister.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.payload;
+      });
+  },
+});
+
+export const { authReset } = authSlice.actions;
+export default authSlice.reducer;
--- frontend/src/features/store.ts
+++ frontend/src/features/store.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./auth/authSlice";
+// import userReducer from "./user/userSlice";
+// import chatReducer from "./chat/chatSlice";
+// import uploadReducer from "./upload/uploadSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+    // user: userReducer,
+    // chat: chatReducer,
+    // upload: uploadReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
