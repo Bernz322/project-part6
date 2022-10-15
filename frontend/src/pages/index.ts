76356e73e6a1b52ee7b92a3d5bdf53a10d761fd9
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-15 21:48:21 +0800

    add index file for exporting pages and components

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,19 @@
+export { default as Welcome } from "./Welcome";
+
+export { default as Login } from "./Login";
+export { default as LoginSuccess } from "./LoginSuccess";
+
+export { default as Register } from "./Register";
+export { default as RegisterSuccess } from "./RegisterSuccess";
+
+export { default as Logout } from "./Logout";
+
+// export { default as GroupChat } from './GroupChat';
+
+// export { default as UsersList } from './UsersList';
+// export { default as EditUser } from './EditUser';
+
+// export { default as DocsList } from './DocsList';
+// export { default as Share } from './Share';
+
+export { default as ErrorPage } from "./ErrorPage";
