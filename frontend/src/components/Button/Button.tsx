d24dcb9476b532a8e3b37a5a7812c0d5b0f46feb
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-15 21:46:30 +0800

    add navbar and button as reusable components

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from "react";
+import classes from "./button.module.scss";
+
+type ButtonProps = {
+  variant?: string;
+  type?: string;
+  text: string;
+  bold?: boolean;
+  loading?: boolean;
+  click?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+const Button: FC<ButtonProps> = ({
+  variant,
+  type,
+  text,
+  bold,
+  loading,
+  click,
+}) => {
+  const btnBgColor = {
+    backgroundColor:
+      variant === "cyan" ? "#66ffff" : variant === "dark" ? "#b3b3b3" : "white",
+    fontWeight: bold ? "700" : "normal",
+    minWidth:
+      type === "unstyled"
+        ? "fit-content"
+        : type === "delete"
+        ? "70px"
+        : type === "add"
+        ? "120px"
+        : "150px",
+    borderRadius: "10px",
+    margin:
+      type === "unstyled"
+        ? "0 5px"
+        : type === "delete"
+        ? "0 4px 0"
+        : type === "add"
+        ? "0 5px"
+        : "30px 0",
+    padding:
+      type === "unstyled"
+        ? "5px 15px"
+        : type === "delete"
+        ? "0 4px 0"
+        : type === "add"
+        ? "3px 10px"
+        : "5px 0",
+  };
+
+  return (
+    <button style={btnBgColor} onClick={click} disabled={loading}>
+      {loading ? <i className={`${classes.loader} fa fa-spinner`}></i> : text}
+    </button>
+  );
+};
+
+export default Button;
---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,61 @@
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { authLogout } from "../../features/auth/authSlice";
+import { useTypedDispatch } from "../../hooks/rtk-hooks";
+import classes from "./navbar.module.scss";
+
+const Navbar = () => {
+  const dispatch = useTypedDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const path = location.pathname.split("/")[1];
+
+  const handleLogout = () => {
+    dispatch(authLogout()).then(() => {
+      navigate("/logout");
+    });
+  };
+
+  return (
+    <nav>
+      <ul>
+        <NavLink
+          className={(navData) =>
+            navData.isActive ? classes.navLinkActive : ""
+          }
+          to="/group-chat"
+        >
+          <li>Group Chat</li>
+        </NavLink>
+        <NavLink
+          className={(navData) =>
+            navData.isActive || path === "edit-user"
+              ? classes.navLinkActive
+              : ""
+          }
+          to="/users-list"
+        >
+          <li>Manage Users</li>
+        </NavLink>
+        <NavLink
+          className={(navData) =>
+            navData.isActive || path === "share" ? classes.navLinkActive : ""
+          }
+          to="/docs-list"
+        >
+          <li>Manage Documents</li>
+        </NavLink>
+        <NavLink
+          className={(navData) =>
+            navData.isActive ? classes.navLinkActive : ""
+          }
+          to="/logout"
+          onClick={handleLogout}
+        >
+          <li>Logout</li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
