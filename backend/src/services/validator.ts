6db636669e00442840e0db3ceadce330442f450b
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-19 17:17:23 +0800

    add validator for email and password

---
+++ backend/src/services/validator.ts
@@ -0,0 +1,17 @@
+import isemail from 'isemail';
+import {HttpErrors} from '@loopback/rest';
+import {CredentialsType} from '../controllers';
+
+export function validateCredentials(credentials: CredentialsType) {
+  // Validate Email
+  if (!isemail.validate(credentials.email)) {
+    throw new HttpErrors.UnprocessableEntity('Invalid email');
+  }
+
+  // Validate Password Length
+  if (!credentials.password || credentials.password.length < 8) {
+    throw new HttpErrors.UnprocessableEntity(
+      'Password must be minimum 8 characters',
+    );
+  }
+}
